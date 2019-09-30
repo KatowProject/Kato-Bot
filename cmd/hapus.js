ca950af4b76fbabf02df9345d40282d4581d8565
Kato <52640561+KatowProject@users.noreply.github.com>
2019-09-30 15:36:58 +0700

    Update hapus.js

--- cmd/hapus.js
+++ cmd/hapus.js
@@ -2,12 +2,12 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
     if(!message.member.hasPermissions("MANAGE_MESSAGES")) return message.reply("Kato takbisa menghapus pesan yang kauinginkan. Maaf, ya ...")
-    if(!args[0]) return message.channel.send("Kato takbisa menghapus pesan yang kauinginkan.")
+    if(!args[0]) return message.channel.send(" Tidak bisa menghapus pesan yang kau inginkan!")
     message.channel.bulkDelete(args[0]).then(() => {
-        message.channel.send(`:ok_hand: ${args[0]} Pesan telah Kato hapus!`).then(message => message.delete(5000));
+        message.channel.send(`:ok_hand: ${args[0]} Pesan telah terhapus!`).then(message => message.delete(5000));
     });
 }
 
 module.exports.help = {
-    name : "hapus"
+    name : "prune"
 }
