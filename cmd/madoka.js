048f9dc4a7375213c7cffc542d37d7de32f0a7e6
Kato <52640561+KatowProject@users.noreply.github.com>
2019-10-01 19:34:07 +0700

    Update madoka.js

--- cmd/madoka.js
+++ cmd/madoka.js
@@ -1,9 +1,10 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-    return message.channel.send("https://cdn.discordapp.com/attachments/447408276628307969/623823526717161473/haveyourpraisedthenameofoursaviourgoddessmadokatoday.png")
+    const attachment = new Discord.Attachment('https://bit.ly/2m1hWPj');
+       
+     message.channel.send(attachment);
 }
-
 module.exports.help = {
     name: "madoka"
   }
