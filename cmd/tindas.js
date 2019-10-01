45bc4e6d8f7cc85118cdc63a5eaa951765e650ac
Kato <52640561+KatowProject@users.noreply.github.com>
2019-10-01 19:39:29 +0700

    Update tindas.js

--- cmd/tindas.js
+++ cmd/tindas.js
@@ -1,9 +1,14 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-    return message.channel.send("https://image.prntscr.com/image/1MLtCWG0QAmI-ouhaEJXDA.png")
+
+    const attachment = new Discord.Attachment('https://image.prntscr.com/image/1MLtCWG0QAmI-ouhaEJXDA.png');
+       
+ message.channel.send(attachment);
 }
 
 module.exports.help = {
     name: "tindas"
   }
+
+  
