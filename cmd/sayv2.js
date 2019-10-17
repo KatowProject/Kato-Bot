8062ac25b974bdd862752e0c87793fabe0cefb8c
Kato <52640561+KatowProject@users.noreply.github.com>
2019-10-17 21:15:24 +0700

    Update sayv2.js

--- cmd/sayv2.js
+++ cmd/sayv2.js
@@ -1,29 +1,30 @@
+// NPM Package = discord.js
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-let string_message = args.slice(0).join(" ");
-if (string_message) {
-    message.delete().catch(O_o=>{});
-    message.channel.send(string_message);
-};
+ let string_message = args.slice(0).join(" ");
+ if (string_message == "") string_message = "Tidak ada keterangan."; 
 
-if (!string_message) {
-    
-    let lastMessage_author = message.author.lastMessageID;
-    let message_id = message.channel.messages.get(`${lastMessage_author}`);
-    let get_attachmentsID = message_id.attachments.map(file => file.id);
-    let attachments_id = message_id.attachments.get(`${get_attachmentsID}`);
-    let hasil = `${attachments_id.url}`;
-    message.delete().catch(O_o=>{});
-    const attachment = new Discord.Attachment(hasil);
-    
-    let channel = message.guild.channels.find("name", "event").send(attachment);
-    let channel = message.guild.channels.find("name", "event").send(`Kiriman Dari ${nessage.author}`)
-    message.channel.send("Gambar Telah Terkirim!")
+ if (string_message) {   
+  
+  // Last Message ID
+   let lastMessage_author = message.author.lastMessageID;
+  // Get Last Message ID
+   let message_id = message.channel.messages.get(`${lastMessage_author}`);
+  // Attachment ID
+   let get_attachmentsID = message_id.attachments.map(file => file.id);
+  // Get Attachment ID
+  let attachments_id = message_id.attachments.get(`${get_attachmentsID}`);
+  // URL Images
+  let hasil = `${attachments_id.url}`;
+   message.delete().catch(O_o=>{});
+
+  // Attach an image
+   message.channel.send(`${string_message}`, {files: [`${hasil}`]});
 };
 
 }
 
 module.exports.help = {
-    name: "submit"
+    name: "say"
 }
