ef08212dec0fd6b6b9f4394e939cf057444ec392
Kato <52640561+KatowProject@users.noreply.github.com>
2019-10-19 16:37:13 +0700

    Update tanya.js

--- cmd/tanya.js
+++ cmd/tanya.js
@@ -7,7 +7,7 @@ var badman = "<:feelsbadman:583214722548105218>"
 
 
 module.exports.run = async (bot, message, args) => {
-    if(!args[2]) return message.reply("Berikan pertanyaan kepada ku!");
+    if(!args[0]) return message.reply("Berikan pertanyaan kepada ku!");
     let balas = ["Ya." , `Tidak ${badman} ` , "Aku tidak mengetahuinya! :triumph:." , `Tanyakan lagi nanti ${pepeok}.` , `Pertanyaan mu gaje ${pepebruh}. ` , `${paansi}.` ];
 
     let hasil = Math.floor((Math.random() * balas.length));
