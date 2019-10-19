8d56d4bf90f21fceb74c6e75190a807b8e62d36b
Kato <52640561+KatowProject@users.noreply.github.com>
2019-10-19 09:52:30 +0700

    Create tanya.js

---
+++ cmd/tanya.js
@@ -0,0 +1,27 @@
+const Discord = require("discord.js")
+
+var pepeok = "<:pepeok:593756215725654053>"
+var pepebruh = "<:pepebruh:597468775041990657>"
+var paansi = "<:paansi:579665763997188096>"
+var badman = "<:feelsbadman:583214722548105218>"
+
+
+module.exports.run = async (bot, message, args) => {
+    if(!args[2]) return message.reply("Berikan pertanyaan kepada ku!");
+    let balas = ["Ya." , `Tidak ${badman} ` , "Aku tidak mengetahuinya! :triumph:." , `Tanyakan lagi nanti ${pepeok}.` , `Pertanyaan mu gaje ${pepebruh}. ` , `${paansi}.` ];
+
+    let hasil = Math.floor((Math.random() * balas.length));
+    let pertanyaan = args.slice(1).join("");
+
+    let embed = new Discord.RichEmbed()
+    .setAuthor(message.author.tag)
+    .setColor("#RANDOM")
+    .addField("Pertanyaan" , pertanyaan)
+    .addField("Jawaban" , balas[hasil])
+
+    message.channel.send(embed)
+}
+    
+    module.exports.help = {
+        name : "tanya"
+    }
