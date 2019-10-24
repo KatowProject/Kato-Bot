a9154047c8fb790728f829c641c8f895fe7a73c0
AnakSoleh <40450165+MzHellcat@users.noreply.github.com>
2019-10-24 19:02:33 +0700

    sedikit perubahan di embed
    deklarasi 'embed' diubah jadi diluar

--- cmd/avatar.js
+++ cmd/avatar.js
@@ -5,43 +5,37 @@ module.exports.run = async (client, message, args) => {
   let target_author = !args[0];
   let target_mentions = message.mentions.users.first();
   let target_server = args[0] === "server";
-
+  
+  const embed = new Discord.RichEmbed()
+     .setColor("#985ce7")
+  
   if (target_server) {
-    let embed = new Discord.RichEmbed()
-    .setColor("#985ce7")
-    .setAuthor(message.guild.name, message.guild.iconURL)
-    .setDescription(`[Avatar URL Link](${message.guild.iconURL}?size=2048)`)
-    .setImage(`${message.guild.iconURL}?size=2048`)
+    embed.setAuthor(message.guild.name, message.guild.iconURL)
+    embed.setDescription(`[Avatar URL Link](${message.guild.iconURL}?size=2048)`)
+    embed.setImage(`${message.guild.iconURL}?size=2048`)
     message.channel.send(embed);
   };
 
   if (target_mentions) {
-    let embed = new Discord.RichEmbed()
-    .setColor("#985ce7")
-    .setAuthor(target_mentions.tag, target_mentions.displayAvatarURL)
-    .setDescription(`[Avatar URL Link](${target_mentions.displayAvatarURL})`)
-    .setImage(target_mentions.displayAvatarURL)
+    embed.setAuthor(target_mentions.tag, target_mentions.displayAvatarURL)
+    embed.setDescription(`[Avatar URL Link](${target_mentions.displayAvatarURL})`)
+    embed.setImage(target_mentions.displayAvatarURL)
     message.channel.send(embed);
   };
 
   if (target_userID) {
-    let embed = new Discord.RichEmbed()
-    .setColor("#985ce7")
-    .setAuthor(target_userID.user.tag, target_userID.user.displayAvatarURL)
-    .setDescription(`[Avatar URL Link](${target_userID.user.displayAvatarURL})`)
-    .setImage(target_userID.user.displayAvatarURL)
+    embed.setAuthor(target_userID.user.tag, target_userID.user.displayAvatarURL)
+    embed.setDescription(`[Avatar URL Link](${target_userID.user.displayAvatarURL})`)
+    embed.setImage(target_userID.user.displayAvatarURL)
     message.channel.send(embed);
   };
 
   if (target_author) {
-    let embed = new Discord.RichEmbed()
-    .setColor("#985ce7")
-    .setAuthor(message.author.tag, message.author.displayAvatarURL)
-    .setDescription(`[Avatar URL Link](${message.author.displayAvatarURL})`)
-    .setImage(message.author.displayAvatarURL)
+    embed.setAuthor(message.author.tag, message.author.displayAvatarURL)
+    embed.setDescription(`[Avatar URL Link](${message.author.displayAvatarURL})`)
+    embed.setImage(message.author.displayAvatarURL)
     message.channel.send(embed);
   };
-
 }
 
 
