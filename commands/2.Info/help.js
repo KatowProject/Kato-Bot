4a26581153ab12c4406d79b287781be1f713fdd9
KatowProject <naufalfaqih443@gmail.com>
2020-07-17 09:07:27 +0700

    ヾ(≧▽≦*)o

--- commands/2.Info/help.js
+++ commands/2.Info/help.js
@@ -7,7 +7,7 @@ exports.run = async (client, message, args) => {
   if (!args[0]) {
     let module = client.helps.array();
     if (!client.config.owners.includes(message.author.id)) module = module.filter(x => !x.hide)
-    const embed = new MessageEmbed().setColor(client.warna.kato).setTimestamp().setFooter(`© 2020, Perkumpulan Orang Santai • Total: ${client.commands.size} commands`, client.user.avatarURL).setDescription(`Ketik \`k!help [command] / @mention help [command]\` untuk menambahkan informasi lebih lanjut mengenai sebuah perintah.`).setTitle('Kato-Bot Command List')
+    const embed = new MessageEmbed().setColor(client.warna.kato).setTimestamp().setFooter(`© 2020, Perkumpulan Orang Santai • Total: ${client.commands.size} commands`, client.user.avatarURL).setDescription(`Ketik \`${client.config.prefix}help [command] / ${client.config.prefix2}help [command]\` untuk menambahkan informasi lebih lanjut mengenai sebuah perintah.`).setTitle('Kato-Bot Command List')
 
     for (const mod of module) {
       embed.addField(`${mod.name}`, mod.cmds.map(x => `\`${x}\``).join(' . '));
