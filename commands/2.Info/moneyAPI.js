30457c5e54fe88ccc17f49a4691f14b9af39dcfd
KatowProject <naufalfaqih443@gmail.com>
2020-09-22 21:57:27 +0700

    new cmd moneyAPI

---
+++ commands/2.Info/moneyAPI.js
@@ -0,0 +1,32 @@
+const Discord = require('discord.js');
+const axios = require('axios');
+
+exports.run = async (client, message, args) => {
+    let api = await axios.get('https://api.exchangeratesapi.io/latest?base=USD'); api = api.data;
+
+    let embed = new Discord.MessageEmbed()
+        .setColor(client.warna.kato)
+        .setAuthor('Currency', 'https://i.gifer.com/ZgRn.gif', 'https://api.exchangeratesapi.io/')
+        .setTitle('Base Currency US$1')
+        .addField('IDR', 'Rp' + api.rates.IDR, true)
+        .addField('JPY', '¥' + api.rates.JPY, true)
+        .addField('EUR', '€' + api.rates.EUR, true)
+        .addField('CNY', '¥' + api.rates.CNY, true)
+        .addField('HKD', 'HK$' + api.rates.HKD, true)
+        .addField('SGD', 'SG$' + api.rates.SGD, true)
+        .setFooter('Date: ' + api.date)
+
+    message.channel.send(embed)
+}
+
+exports.conf = {
+    aliases: [],
+    cooldown: 5
+}
+
+exports.help = {
+    name: 'money',
+    description: 'currency',
+    usage: 'money',
+    example: 'money'
+}
