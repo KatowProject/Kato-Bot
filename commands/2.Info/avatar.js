d92b2d0737bd7289e6d78a6d6b87521cbd948d78
MzHellcat <40450165+MzHellcat@users.noreply.github.com>
2020-09-23 20:29:53 +0700

    username search

--- commands/2.Info/avatar.js
+++ commands/2.Info/avatar.js
@@ -7,14 +7,29 @@ exports.run = async (client, message, args) => {
   let userID = message.guild.members.cache.get(args[0])
   let self = !args[0]
   let server = args[0] === "server"
-  let find = message.channel.guild.members.cache.find(a => {
+  let nick = message.channel.guild.members.cache.find(a => {
     return a.nickname === args.join(' ') ? a.nickname === args.join(' ') : a.user.username === args.join(' ')
+  })
+    let user = message.channel.guild.members.cache.find(a => {                                                   //search via username
+    return a.username === args.join(' ') ? a.nickname === args.join(' ') : a.user.username === args.join(' ')    //in case nicknya null
   })
 
   //embed
   let embed = new Discord.MessageEmbed().setColor(client.warna.kato)
 
   //get avatar and send to user
+    if (nick) {
+    embed.setAuthor(find.user.tag, find.user.displayAvatarURL({ size: 4096, dynamic: true }))
+    embed.setDescription(`[Avatar URL](${find.user.displayAvatarURL({ size: 4096, dynamic: true })})`)
+    embed.setImage(find.user.displayAvatarURL({ size: 4096, dynamic: true }).replace('.webp', '.png'))
+    return message.channel.send(embed);
+  } else;
+    if (user) {                                                                                             //ini untuk username
+    embed.setAuthor(find.user.tag, find.user.displayAvatarURL({ size: 4096, dynamic: true }))               //jadi kalo nick=null
+    embed.setDescription(`[Avatar URL](${find.user.displayAvatarURL({ size: 4096, dynamic: true })})`)      //dia search via username
+    embed.setImage(find.user.displayAvatarURL({ size: 4096, dynamic: true }).replace('.webp', '.png'))
+    return message.channel.send(embed);
+  } else;
   if (mention) {
     embed.setAuthor(mention.tag, mention.displayAvatarURL({ size: 4096, dynamic: true }))   //author embed
     embed.setDescription(`[Avatar URL](${mention.displayAvatarURL({ size: 4096, dynamic: true })})`) //redirect to avatar link
@@ -38,15 +53,7 @@ exports.run = async (client, message, args) => {
     embed.setDescription(`[Avatar URL Link](${message.guild.iconURL({ size: 4096, dynamic: true })})`)
     embed.setImage(message.guild.iconURL({ size: 4096, dynamic: true }).replace('.webp', '.png'))
     return message.channel.send(embed); //send this message to user
-  } else;
-  if (find) {
-    embed.setAuthor(find.user.tag, find.user.displayAvatarURL({ size: 4096, dynamic: true }))
-    embed.setDescription(`[Avatar URL](${find.user.displayAvatarURL({ size: 4096, dynamic: true })})`)
-    embed.setImage(find.user.displayAvatarURL({ size: 4096, dynamic: true }).replace('.webp', '.png'))
-    return message.channel.send(embed);
-  } else return; //Fikri : Gw mending di ignore aja daripada makan processing power lagi
-
-
+  } else return;
 };
 
 exports.conf = {
