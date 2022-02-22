065e67f92bc9eebd570828af6037166fe45a70e5
KatowProject <naufalfaqih443@gmail.com>
2022-02-23 04:31:34 +0700

    test

--- handler/durationManager.js
+++ handler/durationManager.js
@@ -1,19 +1,23 @@
 const donate = require('../database/schema/Donatur');
 
 module.exports = async (client) => {
-    const donaturs = await donate.find({});
-
-    for (const donatur of donaturs) {
-        const timeLatest = Date.now() - donatur.now;
-        if (timeLatest > donatur.duration) {
-            const guild = client.guilds.cache.get(donatur.guild);
-            const member = guild.members.fetch(donatur.userID);
-            if (!member) donatur.remove();
-
-            await member.roles.remove('932997958788608044');
-            await donatur.remove();
-            client.channels.cache.find(a => a.name === 'staff-bot').send({ content: 'true - Donatur' });
-            member.send('Hai durasi role Donatur mu dilepas oleh kato, terima kasih atas dukungannya!');
+    try {
+        const donaturs = await donate.find({});
+        for (const donatur of donaturs) {
+            const timeLatest = Date.now() - donatur.now;
+            if (timeLatest > donatur.duration) {
+                const guild = client.guilds.cache.get(donatur.guild);
+                const member = guild.members.fetch(donatur.userID);
+                if (!member) donatur.remove();
+                console.log(member);
+                await member.roles.remove('932997958788608044');
+                await donatur.remove();
+                client.channels.cache.find(a => a.name === 'staff-bot').send({ content: 'true - Donatur' });
+                member.send('Hai durasi role Donatur mu dilepas oleh kato, terima kasih atas dukungannya!');
+            };
         };
-    };
+    } catch (e) {
+        console.log(e);
+        client.channels.cache.get('932997960923480099').send(`[Error Donatur Manager] ${e.message}`);
+    }
 };
