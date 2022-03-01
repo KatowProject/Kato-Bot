adc43062a0454e8fae38171a6af71fb0ce1cb10f
KatowProject <naufalfaqih443@gmail.com>
2022-03-01 22:00:23 +0700

    fix bug

--- commands/Server/selfroles.js
+++ commands/Server/selfroles.js
@@ -30,7 +30,7 @@ exports.run = async (client, message, args) => {
         )
 
     const r = await message.channel.send({ embeds: [embed], components: [buttons] });
-    const collector = r.channel.createMessageComponentCollector({ filter: m => m.author.id === message.author.id, time: 60000 });
+    const collector = r.channel.createMessageComponentCollector({ filter: m => m.user.id === message.author.id, time: 60000 });
     collector.on('collect', async (f) => {
         await f.deferUpdate();
         switch (f.customId) {
