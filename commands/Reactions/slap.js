2cad32a7df9c28d11157cdd51666ffad0ed12934
KatowProject <naufalfaqih443@gmail.com>
2022-03-02 21:03:52 +0700

    slap

---
+++ commands/Reactions/slap.js
@@ -0,0 +1,43 @@
+const { MessageEmbed } = require("discord.js");
+const axios = require('axios');
+
+exports.run = async (client, message, args) => {
+    try {
+        const response = await axios.get('https://nekos.life/api/v2/img/slap');
+        const hug = response.data;
+
+        let member = message.mentions.members.first();
+        if (!args[0]) return message.reply("mention seseorang untuk melanjutkan!");
+        const embed = new MessageEmbed();
+
+        if (message.author.id === member.user.id) {
+            embed.setTitle('Kamu memeluk diri sendiri 😳')
+            embed.setColor('RANDOM')
+            embed.setImage(hug.url)
+
+            message.channel.send({ embeds: [embed] });
+        } else {
+            embed.setTitle(`${message.guild.members.cache.get(message.author.id).displayName} memeluk ${message.guild.members.cache.get(member.user.id).displayName} (✿◡‿◡)`)
+            embed.setColor('RANDOM')
+            embed.setImage(hug.url)
+
+            message.channel.send({ embeds: [embed] });
+        };
+    } catch (error) {
+        return message.channel.send(`Something went wrong: ${error.message}`);
+        // Restart the bot as usual.
+    }
+}
+
+exports.conf = {
+    aliases: ["tampar"],
+    cooldown: 5,
+    location: __filename
+}
+
+exports.help = {
+    name: 'slap',
+    description: 'reaksi',
+    usage: 'k!slap <user>',
+    example: 'k!slap @juned'
+}
