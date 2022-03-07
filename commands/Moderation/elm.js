47067b0eeabc4403c8d0b14988b688a6769415cb
KatowProject <naufalfaqih443@gmail.com>
2022-03-07 18:49:41 +0700

    fix bug

--- commands/Moderation/elm.js
+++ commands/Moderation/elm.js
@@ -4,8 +4,8 @@ const ELM = require('../../database/schema/ELMs');
 exports.run = async (client, message, args) => {
     try {
 
-        if (!message.guild.me.hasPermission("MUTE_MEMBERS")) return message.channel.send("Aku tidak mempunyai akses!");
-        if (!message.member.hasPermission("MUTE_MEMBERS")) return message.channel.send("Kamu tidak memiliki izin untuk menggunakan perintah ini!");
+        if (!message.guild.me.permissions.has("MUTE_MEMBERS")) return message.channel.send("Aku tidak mempunyai akses!");
+        if (!message.member.permissions.has("MUTE_MEMBERS")) return message.channel.send("Kamu tidak memiliki izin untuk menggunakan perintah ini!");
 
         let elm = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
         if (!elm) return;
