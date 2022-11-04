41170d087dc3d3945cc1a600a6c8f8620058485f
KatowProject <naufalfaqih443@gmail.com>
2022-11-04 17:38:03 +0700

    typo

--- module/Trakteer/tools.js
+++ module/Trakteer/tools.js
@@ -1,45 +1,8 @@
 const baseURL = 'https://trakteer.id/';
 const axios = require('axios');
 
-function get(endpoint, options) {
-    return new Promise(async (resolve, reject) => {
-        try {
-            const res = await axios.get(baseURL + endpoint, {
-                headers: {
-                    cookie: `XSRF-TOKEN=${options['XSRF-TOKEN']}; trakteer-sess=${options['trakteer-id-session']}`
-                }
-            })
 
-            if (res.status === 200) return resolve(res);
-            else reject(res);
-        } catch (err) {
-            const bypass = await bypass(baseURL + endpoint, 'get', {
-                cookie: `XSRF-TOKEN=${options['XSRF-TOKEN']}; trakteer-sess=${options['trakteer-id-session']}`
-            });
-            if (bypass.status === 200) return resolve(bypass);
-            else reject(bypass);
-        }
-    });
-}
-
-function post(json, url) {
-    return new Promise(async (resolve, reject) => {
-        try {
-            const res = await axios.post(url, json, {
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            });
-
-            if (res.status) return resolve(res);
-            else reject(res);
-        } catch (err) {
-            reject(err);
-        }
-    });
-}
-
-function bypass(url, method, headers = {}, data = {}) {
+const bypass = (url, method, headers = {}, data = {}) => {
     return new Promise(async (resolve, reject) => {
         try {
             const bs64 = Buffer.from(url).toString('base64');
@@ -60,5 +23,42 @@ function bypass(url, method, headers = {}, data = {}) {
     });
 }
 
+function get(endpoint, options) {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const res = await axios.get(baseURL + endpoint, {
+                headers: {
+                    cookie: `XSRF-TOKEN=${options['XSRF-TOKEN']}; trakteer-sess=${options['trakteer-id-session']}`
+                }
+            })
+
+            if (res.status === 200) return resolve(res);
+            else reject(res);
+        } catch (err) {
+            const res = await bypass(baseURL + endpoint, 'get', {
+                cookie: `XSRF-TOKEN=${options['XSRF-TOKEN']}; trakteer-sess=${options['trakteer-id-session']}`
+            });
+            if (res.status === 200) return resolve(res);
+            else reject(res);
+        }
+    });
+}
+
+function post(json, url) {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const res = await axios.post(url, json, {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+
+            if (res.status) return resolve(res);
+            else reject(res);
+        } catch (err) {
+            reject(err);
+        }
+    });
+}
 
 module.exports = { get, post };
